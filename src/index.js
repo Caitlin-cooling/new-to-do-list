eac4179454f5b41e88defa3ad8b6096498235f8f
Caitlin Cooling <ccooling97@gmail.com>
2019-01-14 15:49:11 +0000

    Input the HTML webpack plugin

--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
 import React from "react";
 import ReactDOM from "react-dom";
+//
+// var Index = function() {
+//   return <div>Hello React!</div>;
+// };
 
-const Index = () => {
-  return <div>Hello React!</div>;
-};
-
-ReactDOM.render(<Index />, document.getElementById("index"));
+ReactDOM.render(<div>Hello React!</div>, document.getElementById("index"));
