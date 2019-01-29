99bc5dcbfd23541553ed0b53d367f475c188694d
Caitlin Cooling <ccooling97@gmail.com>
2019-01-29 17:10:36 +0000

    ToDoList is now a class component
    - To track state of clicking 
    - Set up to allow user to mark of a todo

--- src/todolist.js
+++ src/todolist.js
@@ -1,9 +1,17 @@
 import './global.js';
 
-export var ToDoList = function(props) {
-  let listItems;
+export class ToDoList extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      clicked: false,
+      listItems: [],
+      list: props.list
+    };
+    this.handleClick = this.handleClick.bind(this);
+  }
 
-  function formatDate(date) {
+  formatDate(date) {
     var dd = date.getDate();
     var mm = date.getMonth() + 1;
 
@@ -17,6 +25,15 @@ export var ToDoList = function(props) {
     return dd + '/' + mm + '/' + yyyy;
   }
 
-  listItems = props.list.map((todo, i) => <li key={'todo-' + i} >{todo['title']} is due {formatDate(todo['date'])}</li>)
-  return <ul>{listItems}</ul>
+  handleClick(e) {
+    this.setState({clicked: true})
+  }
+
+  render() {
+    this.state.listItems = this.state.list.map((todo, i) =>
+            <li key={'todo-' + i} >
+              {todo['title']} is due {this.formatDate(todo['date'])}
+            </li>)
+    return (<ul>{this.state.listItems}</ul>)
+  }
 }
