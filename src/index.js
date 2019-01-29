82a22e4a25a1779ae4aac69e76c850f8bdda004c
Caitlin Cooling <ccooling97@gmail.com>
2019-01-29 15:29:12 +0000

    Refactor
    - CreateForm is now in it's own file for readiblity

--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import { HomePage } from "./homepage.js";
-import DatePicker from "react-datepicker";
-import { Navbar, Nav, NavItem, Button } from "react-bootstrap";
+import { CreateForm } from "./createform.js";
+import { Navbar, Nav, NavItem } from "react-bootstrap";
 import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
 import style from "react-datepicker/dist/react-datepicker.css";
 import './index.css';
@@ -77,20 +77,6 @@ class App extends React.Component {
   }
 }
 
-var CreateForm = function(props) {
-  return <div className='center'>
-          <form id='create-form'>
-            <FormTitle/>
-            <div>
-              <Title title={props.title} handleTitleChange={props.handleTitleChange}/>
-              <DueDate dueDate={props.dueDate} handleDateChange={props.handleDateChange}/>
-              <br/>
-              <SubmitButton handleClick={props.handleClick}/>
-            </div>
-        </form>
-      </div>
-    }
-
 var NavBar = function() {
   return <Navbar>
     <Nav>
@@ -121,39 +107,6 @@ var Shapes = function() {
       </div>
     </div>
   </div>
-}
-
-var FormTitle = function() {
-  return <h1>Add a new to do</h1>
-}
-
-var Title = function(props) {
-  return <p>
-    <input className="text-field"
-      type="text"
-      placeholder="Title"
-      value={props.title}
-      onChange={props.handleTitleChange}/>
-    </p>
-}
-
-var DueDate = function (props) {
-  return <div>
-    <DatePicker
-      className="text-field"
-      todayButton={"Today"}
-      placeholderText="Click to select a date"
-      dateFormat="dd/MM/yyyy"
-      selected={props.dueDate}
-      onChange={props.handleDateChange}
-    />
-  </div>
-}
-
-var SubmitButton = function(props) {
-  return <Button onClick={props.handleClick}>
-    <Link className='link' to="/">Submit</Link>
-  </Button>
 }
 
 ReactDOM.render(
