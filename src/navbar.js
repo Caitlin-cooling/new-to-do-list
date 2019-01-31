8f86f67dc0ebdba707261b7af8bb5b0c2942bc9b
Caitlin Cooling <ccooling97@gmail.com>
2019-01-31 15:51:59 +0000

    Wrap navBar in Router to enable the use of Links

--- src/navbar.js
+++ src/navbar.js
@@ -2,21 +2,25 @@ import './global.js';
 import { Navbar, Nav, NavItem } from "react-bootstrap";
 import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
 
-export var NavBar = function() {
-  return <Navbar>
-    <Nav>
-      <Navbar.Header>
-        <Navbar.Brand>
-        <Link className="home-button" to="/">
-          Do it
-        </Link>
-        </Navbar.Brand>
-      </Navbar.Header>
-      <NavItem>
-        <Link className="new-button" to="/new">
-          Create new
-        </Link>
-      </NavItem>
-    </Nav>
-  </Navbar>
+export class NavBar extends React.Component {
+  render() {
+    return <Router>
+      <Navbar>
+        <Nav>
+          <Navbar.Header>
+            <Navbar.Brand>
+            <Link className="home-button" to="/">
+              Do it
+            </Link>
+            </Navbar.Brand>
+          </Navbar.Header>
+          <NavItem>
+            <Link className="new-button" to="/new">
+              Create new
+            </Link>
+          </NavItem>
+        </Nav>
+      </Navbar>
+    </Router>
+  }
 }
